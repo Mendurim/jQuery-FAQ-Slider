815cfe775ed17824d3e8d76caf962a26729af65a
mendurim <mendurim.r@gmail.com>
2016-03-10 20:52:38 +0100

    Added the document ready function and slideUp

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var action = "click";
 var speed = "500"; 
 
 $(document).ready(function(){
-	$('.li.q').on(action, function(){
-		
+	$('li.q').on(action, function(){
+		$(this).next().slideToggle(speed).siblings('li.a').slideUp(); 
 	}); 
 }); 
